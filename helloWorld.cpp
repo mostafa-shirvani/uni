6e183a8d5cfc8518d6d85b221a4dbe8eb8e81001
mostafa-shirvani <shirvani13811381@gmail.com>
2025-03-06 01:33:48 +0330

    first commit

---
+++ helloWorld.cpp
@@ -0,0 +1,5 @@
+//
+// Created by AsreRayane on 3/6/2025.
+//
+
+#include "helloWorld.h"
---
+++ helloWorld.h
@@ -0,0 +1,14 @@
+//
+// Created by AsreRayane on 3/6/2025.
+//
+
+#ifndef UNI2_HELLOWORLD_H
+#define UNI2_HELLOWORLD_H
+
+
+class helloWorld {
+
+};
+
+
+#endif //UNI2_HELLOWORLD_H
